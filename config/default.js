a491c8ad1f479ef9219c2fd8b3989cb79edffc81
Mithun Kamath <callmekatootie@gmail.com>
2018-04-14 23:10:58 +0530

    Added ssl certs for kafka

--- config/default.js
+++ config/default.js
@@ -1,6 +1,9 @@
 /**
  * The configuration file.
  */
+
+const fs = require('fs');
+
 module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   PORT: process.env.PORT || 4000,
@@ -9,6 +12,6 @@ module.exports = {
   KAFKA_URL: process.env.KAFKA_URL || 'localhost:9092',
   // below are used for secure Kafka connection, they are optional
   // for the local Kafka, they are not needed
-  KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT,
-  KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY,
+  KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT || fs.readFileSync('./kafka_client.crt'),
+  KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY || fs.readFileSync('./kafka_client.key'),
 };
